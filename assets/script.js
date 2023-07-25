44de044ae7da62edfe072a4564749fcc81577ab8
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-07-24 20:45:35 -0700

    edit the API

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
-var APIkey = bfd9e7606a4c20b7da2609119a710775;
+var APIkey = "bfd9e7606a4c20b7da2609119a710775";
+
+var currentCity;
 
 function weatherInfo(data) {
   var url =
-    "https://api.openweathermap.org/data/3.0/onecall?lat=${data.lat}&lon=${data.lon}&exclude=alerts&units&appid={APIkey}";
+    "https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${" +
+    APIkey +
+    "}";
 
   // Get Data From URL Provided
   fetch(url)
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {});
+    .then(function (data) {
+      // Current Weather
+      var today = document.getElementById("current-temp");
+    });
 }
