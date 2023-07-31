01d0ad8ed75d48af5ce73447361981d50de67ab9
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-07-31 02:03:41 -0700

    nope

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,7 @@ function handleSearchFormSubmit(event) {
 
   if (userInputData) {
     const cities = JSON.parse(userInputData);
-    cities.push({ name: currentCity }); // cities.push is not a function in dev tools
+    cities.push({ name: currentCity }); // Still not a function
     localStorage.setItem("userInputData", JSON.stringify(cities));
   } else {
     localStorage.setItem(
